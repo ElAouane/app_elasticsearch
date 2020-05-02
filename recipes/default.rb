cdb314cf9db8ae8d18d513cd392327200dc7fac4
elaouane <hamzagamedev@hotmail.com>
2020-05-02 03:53:16 +0100

    fix

--- recipes/default.rb
+++ recipes/default.rb
@@ -45,9 +45,19 @@ execute 'fix_template' do
   command 'sudo chmod go-w /etc/elasticsearch/elasticsearch.yml'
 end
 
-package "kibana" do
-  action :install
+execute "add key" do
+  command 'wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -'
 end
+
+execute "add repository" do
+  command 'sudo add-apt-repository "deb https://artifacts.elastic.co/packages/7.x/apt stable main"'
+end
+
+apt_update "update" do
+  action :update
+end
+
+package "kibana"
 
 template 'etc/kibana/kibana.yml' do
   source 'kibana.yml.erb'
